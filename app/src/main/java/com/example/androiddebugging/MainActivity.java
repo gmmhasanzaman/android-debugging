c2f740a1a2398925d04fe8c3854f7ca6a24d5eb5
HASAN <gmmhasan2017@gmail.com>
2020-01-06 14:19:47 +0600

    start debugging

--- app/src/main/java/com/example/androiddebugging/MainActivity.java
+++ app/src/main/java/com/example/androiddebugging/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.androiddebugging;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView averageTV;
+    private Button calculateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        averageTV = findViewById(R.id.averageTV);
+        calculateBtn = findViewById(R.id.calculateBtn);
+
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int[] numbers = new int[] {3,2,4};
+                double average = getAverage(numbers);
+
+                averageTV.setText("Average: "+average);
+            }
+        });
+    }
+
+    private double getAverage(int[] numbers) {
+
+        int sum = 0;
+
+        for (int i = 0; i <numbers.length; i++){
+            sum = numbers[i];
+        }
+
+        double result = (double)sum / numbers.length;
+
+        return result;
     }
 }
